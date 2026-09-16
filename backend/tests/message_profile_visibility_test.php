727a847cdc0e20cc5a1682ec3c2ac51a5f5fc625
sreekanthab <chethanabvk@gmail.com>
2026-09-16 10:09:23 +0530

    test: correct message profile visibility regression

--- backend/tests/message_profile_visibility_test.php
+++ backend/tests/message_profile_visibility_test.php
@@ -12,7 +12,6 @@ assert(str_contains($handler, 'fr.requester_id=? AND fr.recipient_id=m.sender_id
 assert(str_contains($handler, 'fr.requester_id=m.sender_id AND fr.recipient_id=?'));
 assert(str_contains($handler, 'END AS show_profile'));
 assert(str_contains($handler, '$user[\'id\'], $user[\'id\'], $user[\'id\'], $clearedThrough'));
-assert(str_contains($handler, '$user[\'id\'],$user[\'id\'],$user[\'id\'],$clearedThrough'));
 
 assert(str_contains($mobile, 'message.show_profile && message.sender_id'));
 assert(substr_count($mobile, '{profileAction}') >= 4);
